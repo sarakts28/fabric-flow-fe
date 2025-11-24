924c21df2b55a4be03dcea6627299153069c7d77
QaisCh <136905641+AliChaudary@users.noreply.github.com>
2025-11-24 18:24:44 +0500

    changes done

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://unlacquered-nonanimatingly-agatha.ngrok-free.dev",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
