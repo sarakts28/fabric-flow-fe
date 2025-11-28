fa48310b54a8e15d901e11c54d42bedb2ec57ca4
QaisCh <136905641+AliChaudary@users.noreply.github.com>
2025-11-28 22:19:34 +0500

    changes done

--- src/lib/api.js
+++ src/lib/api.js
@@ -8,59 +8,59 @@ const API = axios.create({
   },
 });
 
-// 🔄 Prevent infinite refresh loops
-let isRefreshing = false;
-let failedQueue = [];
+// // 🔄 Prevent infinite refresh loops
+// let isRefreshing = false;
+// let failedQueue = [];
 
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
+// const processQueue = (error, token = null) => {
+//   failedQueue.forEach((prom) => {
+//     if (error) {
+//       prom.reject(error);
+//     } else {
+//       prom.resolve(token);
+//     }
+//   });
 
-  failedQueue = [];
-};
+//   failedQueue = [];
+// };
 
-// ✨ Response Interceptor
-API.interceptors.response.use(
-  (res) => res,
-  async (err) => {
-    const originalRequest = err.config;
+// // ✨ Response Interceptor
+// API.interceptors.response.use(
+//   (res) => res,
+//   async (err) => {
+//     const originalRequest = err.config;
 
-    // If token expired
-    if (err.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise(function (resolve, reject) {
-          failedQueue.push({ resolve, reject });
-        })
-          .then(() => API(originalRequest))
-          .catch((err) => Promise.reject(err));
-      }
+//     // If token expired
+//     if (err.response?.status === 401 && !originalRequest._retry) {
+//       if (isRefreshing) {
+//         return new Promise(function (resolve, reject) {
+//           failedQueue.push({ resolve, reject });
+//         })
+//           .then(() => API(originalRequest))
+//           .catch((err) => Promise.reject(err));
+//       }
 
-      originalRequest._retry = true;
-      isRefreshing = true;
+//       originalRequest._retry = true;
+//       isRefreshing = true;
 
-      try {
-        // Hit your refresh token endpoint
-        await API.post("/refresh");
+//       try {
+//         // Hit your refresh token endpoint
+//         await API.post("/refresh");
 
-        processQueue(null);
-        isRefreshing = false;
+//         processQueue(null);
+//         isRefreshing = false;
 
-        // Retry original API call
-        return API(originalRequest);
-      } catch (refreshError) {
-        processQueue(refreshError, null);
-        isRefreshing = false;
-        return Promise.reject(refreshError);
-      }
-    }
+//         // Retry original API call
+//         return API(originalRequest);
+//       } catch (refreshError) {
+//         processQueue(refreshError, null);
+//         isRefreshing = false;
+//         return Promise.reject(refreshError);
+//       }
+//     }
 
-    return Promise.reject(err);
-  }
-);
+//     return Promise.reject(err);
+//   }
+// );
 
 export default API;
